81d3f0e725715acacd98fb6b7800abbadc011c62
zach romer <romerzs14@gmail.com>
2020-03-06 15:54:59 -0800

    Modified to launch at 0.0.0.0 when in docker, otherwise launch at 127.0.0.1

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,6 @@ COPY dockerrequirements.txt .
 RUN pip install --upgrade pip
 RUN pip install -r /dockerrequirements.txt
 
+ENV DOCKER_EXE=True
+
 ENTRYPOINT ["python", "main.py"]
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from argparse import ArgumentParser
+import os
 
 if __name__ == '__main__':
     from src.app import api
-    api.app.run(host='127.0.0.1')
+    if os.getenv('DOCKER_EXE'):
+        api.app.run(host='0.0.0.0')
+    else:
+        api.app.run(host='127.0.0.1')
